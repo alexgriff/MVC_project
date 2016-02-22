d7286abeeeb4d46773af0e2608a4f7a47a0355e1
alexgriff <alexalexgriffith@gmail.com>
2016-02-22 16:39:52 -0500

    Run from root no DB error

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source "https://rubygems.org"
 
 # gem "rails"
 gem 'pry-byebug'
+gem 'sqlite3'
 gem 'require_all'
 gem 'rspec'
--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,15 @@
-require 'sqlite3'
 require 'bundler/setup'
 Bundler.require
 
 
 
-DB = {:conn => SQLite3::Database.new("../DB/project.db")}
+DB = {:conn => SQLite3::Database.new("DB/project.db")}
 # drop all tables if exist
-sql = File.read("../DB/drop.sql")
+sql = File.read("DB/drop.sql")
 DB[:conn].execute_batch(sql)
 
 # creat all tables
-sql = File.read("../DB/create.sql")
+sql = File.read("DB/create.sql")
 DB[:conn].execute_batch(sql)
 DB[:conn].results_as_hash = true
 
@@ -18,4 +17,4 @@ DB[:conn].results_as_hash = true
 # require all after establishing connection to
 # database so interactive_record.rb has the DB
 # constant
-require_all '../app'
+require_all 'app'
