b87e6b11b0b744cb958f060be988116407fc90f1
alexgriff <alexalexgriffith@gmail.com>
2016-02-22 18:10:03 -0500

    Put cross-ref methods into Populate model and rename

--- app/concerns/populate.rb
+++ app/concerns/populate.rb
@@ -1,37 +1,88 @@
 require 'pry'
 module Populate
 
-  # Examples:
-  # called on an instance of resource that is being added to a movie:
-  # director.genres = populate("genres", "directors")
-  # actor.directors = populate("director", "actors")
-  #
-  # Note that if director is the resource it is singular, not plural
-  # because a Movie-object has a .director method
-  def populate(resource, current_class)
-    self.movies.map do |title|
-      found = Movie.find_or_create_by_name(title).send(resource)
-      
-      if found.is_a? Array
-        array_populate(found, current_class)
-      elsif found
-        single_elem_populate(found, current_class)
-      end
-
-    end.compact.flatten.uniq
+  def populate_directors_genres_from_add_director(director)
+    # GET GENRE IDs ASSSOC. WITH DIRECTOR
+    #  [{"genre_id"=>1, 0=>1}, {"genre_id"=>4, 0=>4}]
+    sql = <<-SQL
+      SELECT genre_id FROM movies_genres
+      INNER JOIN movies ON movies.id = movies_genres.movie_id
+      WHERE movies.director_id = ?
+    SQL
+    id_hash = DB[:conn].execute(sql, director.id)
+    id_nums = id_hash.map { |row_hash| row_hash.values.first }
+    sql = <<-SQL
+      INSERT INTO directors_genres (director_id,genre_id)
+      VALUES (?, ?)
+    SQL
+    id_nums.each { |num| DB[:conn].execute(sql, director.id, num) }
   end
 
+def populate_directors_genres_from_add_genre(genre)
+    dir_id = self.director_id
+    if dir_id
+      sql = "INSERT INTO directors_genres (director_id, genre_id) VALUES (?, ?)"
+      DB[:conn].execute(sql, dir_id, self.id)
+    end
+end
   
 
-  def array_populate(arr, current_class)
-    arr.each {|elem| elem.send(current_class) << self.name }
-    arr.map { |elem| elem.name}
+  def populate_actors_directors_from_add_actor(actor)
+    dir_id = self.director_id
+    if dir_id
+      sql = "INSERT INTO actors_directors (actor_id, director_id) VALUES (?, ?);"
+      DB[:conn].execute(sql, self.id, dir_id)
+    end
   end
 
-  def single_elem_populate(elem, current_class)
-    elem.send(current_class) << self.name
-    elem.name
+  def populate_actors_directors_from_add_director(director)
+    # GET GENRE IDs ASSSOC. WITH DIRECTOR
+    #  [{"genre_id"=>1, 0=>1}, {"genre_id"=>4, 0=>4}]
+    sql = <<-SQL
+      SELECT actor_id FROM movies_actors
+      INNER JOIN movies ON movies.id = movies_actors.movie_id
+      WHERE movies.director_id = ?
+    SQL
+    id_hash = DB[:conn].execute(sql, director.id)
+    id_nums = id_hash.map { |row_hash| row_hash.values.first }
+    sql = <<-SQL
+      INSERT INTO actors_directors (director_id,actor_id)
+      VALUES (?, ?)
+    SQL
+    id_nums.each { |num| DB[:conn].execute(sql, director.id, num) }
   end
+
+  def populate_actors_genres_from_add_actor(actor)
+    sql = <<-SQL
+      SELECT genre_id FROM movies_genres
+      JOIN movies ON movies.id = movies_genres.movie_id
+      WHERE movies_genres.movie_id = ?
+    SQL
+    id_hash = DB[:conn].execute(sql, self.id)
+    id_nums = id_hash.map { |row_hash| row_hash.values.first }
+    sql = <<-SQL
+      INSERT INTO actors_genres (actor_id, genre_id)
+      VALUES (?, ?)
+    SQL
+    id_nums.each { |num| DB[:conn].execute(sql, actor.id, num) }
+  end
+
+  def populate_actors_genres_from_add_genre(genre)
+    sql = <<-SQL
+      SELECT actor_id FROM movies_actors
+      JOIN movies ON movies.id = movies_actors.movie_id
+      WHERE movies_actors.movie_id = ?
+    SQL
+    id_hash = DB[:conn].execute(sql, self.id)
+    id_nums = id_hash.map { |row_hash| row_hash.values.first }
+    sql = <<-SQL
+      INSERT INTO actors_genres (actor_id, genre_id)
+      VALUES (?, ?)
+    SQL
+    id_nums.each { |num| DB[:conn].execute(sql, num, genre.id) }
+  end
+
+
 
 end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,7 @@ class Movie < InteractiveRecord
   extend Findable
   include Printable
   include Deletable
+  include Populate
 
 
   # attr_accessor :rating, :title, :actors, :genres, :director
@@ -41,204 +42,56 @@ class Movie < InteractiveRecord
     info_hashes.map { |row_hash| row_hash.values.first }
   end
 
+  def director
+    sql = <<-SQL
+      SELECT directors.name FROM directors
+      INNER JOIN movies ON movies.director_id = directors.id
+      WHERE directors.id = ?
+    SQL
+     DB[:conn].execute(sql, self.director_id)[0][0]
+  end
+
   def self.all
     @@all
   end
 
-  def self.delete_by_name(name)
-    self.find_by_name
+  def self.delete_by_name(delete_name)
+    #
   end
-
-  def cross_ref_directors_genres(director)
-    # GET GENRE IDs ASSSOC. WITH DIRECTOR
-    #  [{"genre_id"=>1, 0=>1}, {"genre_id"=>4, 0=>4}]
-    sql = <<-SQL
-      SELECT genre_id FROM movies_genres
-      INNER JOIN movies ON movies.id = movies_genres.movie_id
-      WHERE movies.director_id = ?
-    SQL
-    id_hash = DB[:conn].execute(sql, director.id)
-    id_nums = id_hash.map { |row_hash| row_hash.values.first }
-    sql = <<-SQL
-      INSERT INTO directors_genres (director_id,genre_id)
-      VALUES (?, ?)
-    SQL
-    id_nums.each { |num| DB[:conn].execute(sql, director.id, num) }
-  end
-
-def cross_ref_genres_directors(genre)
-    # GET GENRE IDs ASSSOC. WITH DIRECTOR
-    #  [{"genre_id"=>1, 0=>1}, {"genre_id"=>4, 0=>4}]
-    # sql = <<-SQL
-    #   SELECT genre_id FROM movies_genres
-    #   INNER JOIN movies ON movies.id = movies_genres.movie_id
-
-    #   WHERE movies.director_id = ?
-    # SQL
-    # id_hash = DB[:conn].execute(sql, director.id)
-    # id_nums = id_hash.map { |row_hash| row_hash.values.first }
-    # sql = <<-SQL
-    #   INSERT INTO directors_genres (director_id,genre_id)
-    #   VALUES (?, ?)
-    # SQL
-    # id_nums.each { |num| DB[:conn].execute(sql, director.id, num) }
-
-    dir_id = self.director_id
-    if dir_id
-      sql = "INSERT INTO directors_genres (director_id, genre_id) VALUES (?, ?)"
-      DB[:conn].execute(sql, dir_id, self.id)
-    end
-  end
-  # UPDATE movies
-  # SET director_id = (SELECT id FROM directors
-  #                    WHERE name = directors.name)
-  # WHERE id = self.id
-  # def update
-  #   sql = <<-SQL
-  #     UPDATE movies
-  #     SET #{set_info}
-  #     WHERE id = ?
-  #   SQL
-  #   DB[:conn].execute(sql, *values_array, self.id)
-  #   self
-  # end
 
   def add_director(director)
-    # @director = director
     self.director_id = director.id
     self.update
-    cross_ref_directors_genres(director)
-    cross_ref_directors_actors(director)
-
-    # director.add_movie(self.title)
-
-    # directors_genres
-    # INSERT OR REPLACE INTO directors_genres (genre_id, director_id)
-    # VALUES (SELECT genre_id FROM directors_genres WHERE
-    # director_id = director.id, director.id)
-
-    # director.genres = director.populate("genres", "directors")
-
-    # actors_directors
-    # INSERT OR REPLACE INTO actors_directors (actor_id, director_id)
-    # VALUES (SELECT actor_id FROM actors_directors WHERE
-    # director_id = director.id, director.id)
-    # director.actors = director.populate("actors", "directors")
+    populate_directors_genres_from_add_director(director)
+    populate_actors_directors_from_add_director(director)
   end
 
-  # INSERT INTO movies_actors (movie_id, director_id)
-  # VALUES (self.id, SELECT id FROM directors WHERE
-  # name = director.name)
   def add_actor(actor)
-    # self.actors << actor
-    # actor.add_movie(self.title)
     sql = <<-SQL
       INSERT INTO movies_actors (movie_id, actor_id)
       VALUES (?,?)
     SQL
     DB[:conn].execute(sql, self.id, actor.id)
 
-    cross_ref_actors_directors(actor)
-    cross_ref_actors_genres(actor)
-    # actor.genres = actor.populate("genres", "actors")
-    # actor.directors = actor.populate("director", "actors")
+    populate_actors_directors_from_add_actor(actor)
+    populate_actors_genres_from_add_actor(actor)
   end
 
 
-  # INSERT INTO movies_genres (movie_id, genre_id)
-  # VALUES (self.id, SELECT id FROM genres WHERE
-  # name = genre.name)
   def add_genre(genre)
-    # self.genres << genre
-    # genre.add_movie(self.title)
     sql = <<-SQL
       INSERT INTO movies_genres (movie_id, genre_id)
       VALUES (?,?)
     SQL
     DB[:conn].execute(sql, self.id, genre.id)
 
-    cross_ref_genres_directors(genre)
-    cross_ref_genres_actors(genre)
-    # genre.directors = genre.populate("director", "genres")
-    # genre.actors = genre.populate("actors", "genres")
-    # cross_ref_directors_genres
+    populate_directors_genres_from_add_genre(genre)
+    populate_actors_genres_from_add_genre(genre)
   end
 
   def name
     @title
   end
 
-  def cross_ref_actors_directors(actor)
-
-    dir_id = self.director_id
-    if dir_id
-      sql = "INSERT INTO actors_directors (actor_id, director_id) VALUES (?, ?);"
-      DB[:conn].execute(sql, self.id, dir_id)
-    end
-  end
-
-  def cross_ref_directors_actors(director)
-    # GET GENRE IDs ASSSOC. WITH DIRECTOR
-    #  [{"genre_id"=>1, 0=>1}, {"genre_id"=>4, 0=>4}]
-    sql = <<-SQL
-      SELECT actor_id FROM movies_actors
-      INNER JOIN movies ON movies.id = movies_actors.movie_id
-      WHERE movies.director_id = ?
-    SQL
-    id_hash = DB[:conn].execute(sql, director.id)
-    id_nums = id_hash.map { |row_hash| row_hash.values.first }
-    sql = <<-SQL
-      INSERT INTO actors_directors (director_id,actor_id)
-      VALUES (?, ?)
-    SQL
-    id_nums.each { |num| DB[:conn].execute(sql, director.id, num) }
-  end
-
-#   CREATE TABLE actors_genres (
-#   id INTEGER PRIMARY KEY,
-#   actor_id INTEGER,
-#   genre_id INTEGER
-# );
-
-  def cross_ref_actors_genres(actor)
-    # have movie and actor
-    # go to movies_genres where movie.id
-    # get genre_id**s** 
-    # insert the actor id and genre id
-
-    sql = <<-SQL
-      SELECT genre_id FROM movies_genres
-      JOIN movies ON movies.id = movies_genres.movie_id
-      WHERE movies_genres.movie_id = ?
-    SQL
-    id_hash = DB[:conn].execute(sql, self.id)
-    id_nums = id_hash.map { |row_hash| row_hash.values.first }
-    sql = <<-SQL
-      INSERT INTO actors_genres (actor_id, genre_id)
-      VALUES (?, ?)
-    SQL
-    id_nums.each { |num| DB[:conn].execute(sql, actor.id, num) }
-  end
-
-  def cross_ref_genres_actors(genre)
-    # have movie and genre
-    # go to movies_actors where movie.id
-    # get actor_id**s** 
-    # insert the genre id and actor id
-
-    sql = <<-SQL
-      SELECT actor_id FROM movies_actors
-      JOIN movies ON movies.id = movies_actors.movie_id
-      WHERE movies_actors.movie_id = ?
-    SQL
-    id_hash = DB[:conn].execute(sql, self.id)
-    id_nums = id_hash.map { |row_hash| row_hash.values.first }
-    sql = <<-SQL
-      INSERT INTO actors_genres (actor_id, genre_id)
-      VALUES (?, ?)
-    SQL
-    id_nums.each { |num| DB[:conn].execute(sql, num, genre.id) }
-  end
-
-
+  
 end
