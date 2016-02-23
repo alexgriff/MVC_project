50459cb1041d5e6db837f86df66d4fe7e3be536a
alexgriff <alexalexgriffith@gmail.com>
2016-02-23 08:34:33 -0500

    Remove not-needed joins in Populate module

--- app/concerns/populate.rb
+++ app/concerns/populate.rb
@@ -2,14 +2,11 @@ require 'pry'
 module Populate
 
   def populate_directors_genres_from_add_director(director)
-    # GET GENRE IDs ASSSOC. WITH DIRECTOR
-    #  [{"genre_id"=>1, 0=>1}, {"genre_id"=>4, 0=>4}]
     sql = <<-SQL
-      SELECT genre_id FROM movies_genres
-      INNER JOIN movies ON movies.id = movies_genres.movie_id
-      WHERE movies.director_id = ?
+      SELECT movies_genres.genre_id FROM movies_genres
+      WHERE movies_genres.movie_id = ?
     SQL
-    id_hash = DB[:conn].execute(sql, director.id).uniq
+    id_hash = DB[:conn].execute(sql, self.id).uniq
     id_nums = id_hash.map { |row_hash| row_hash.values.first }
     sql = <<-SQL
       INSERT INTO directors_genres (director_id,genre_id)
@@ -35,14 +32,11 @@ module Populate
   end
 
   def populate_actors_directors_from_add_director(director)
-    # GET GENRE IDs ASSSOC. WITH DIRECTOR
-    #  [{"genre_id"=>1, 0=>1}, {"genre_id"=>4, 0=>4}]
     sql = <<-SQL
-      SELECT actor_id FROM movies_actors
-      INNER JOIN movies ON movies.id = movies_actors.movie_id
-      WHERE movies.director_id = ?
+      SELECT movies_actors.actor_id FROM movies_actors
+      WHERE movies_actors.movie_id = ?
     SQL
-    id_hash = DB[:conn].execute(sql, director.id).uniq
+    id_hash = DB[:conn].execute(sql, self.id).uniq
     id_nums = id_hash.map { |row_hash| row_hash.values.first }
     sql = <<-SQL
       INSERT INTO actors_directors (director_id,actor_id)
@@ -52,35 +46,20 @@ module Populate
   end
 
   def populate_actors_genres_from_add_actor(actor)
-    # sql = <<-SQL
-    #   INSERT INTO actors_genres (actor_id, genre_id)
-    #   VALUES (?, (SELECT genre_id FROM movies_genres
-    #     WHERE movie_id = ?));
-    # SQL
-    # DB[:conn].execute(sql, actor.id, self.id)
     sql = <<-SQL
       SELECT movies_genres.genre_id FROM movies_genres
       WHERE movies_genres.movie_id = ?
     SQL
-    # binding.pry
     id_hash = DB[:conn].execute(sql, self.id).uniq
     id_nums = id_hash.map { |row_hash| row_hash.values.first }
     sql = <<-SQL
       INSERT INTO actors_genres (actor_id, genre_id)
       VALUES (?, ?)
     SQL
-    # binding.pry
     id_nums.each { |num| DB[:conn].execute(sql, actor.id, num) }
   end
 
   def populate_actors_genres_from_add_genre(genre)
-    # sql = <<-SQL
-    #   INSERT INTO actors_genres (genre_id, actor_id)
-    #   VALUES (?, (SELECT actor_id FROM movies_actors
-    #     WHERE movie_id = ?));
-    # SQL
-    # DB[:conn].execute(sql, genre.id, self.id)
-    # sql = <<-SQL
     sql = <<-SQL
       SELECT movies_actors.actor_id FROM movies_actors
       WHERE movies_actors.movie_id = ?
